fd0ff420d2c2451f0b9567054137e53971c72865
Michel Ferreira <michel.ferreira.melo@gmail.com>
2022-04-20 22:24:20 -0300

    adicionando o script de descriptografia

---
+++ scripts/descriptografa.js
@@ -0,0 +1,42 @@
+function descriptografa(){
+    
+    var [j,k] = [0,0];
+    var textFinal = '';
+    var stringTextoPrincipal = textPrincipal.value
+
+    while(j < stringTextoPrincipal.length){
+        if(stringTextoPrincipal[k] === 'a'){
+            textFinal += 'a';
+            k += 2
+            j = k
+
+        }else if(stringTextoPrincipal[k] === 'e'){
+            textFinal += 'e';
+            k += 5
+            j = k
+
+        }else if(stringTextoPrincipal[k] === 'i'){
+            textFinal += 'i';
+            k += 4
+            j = k
+
+        }else if(stringTextoPrincipal[k] === 'o'){
+            textFinal += 'o';
+            k += 4
+            j = k
+
+        }else if(stringTextoPrincipal[k] === 'u'){
+            textFinal += 'u';
+            k += 4
+            j = k
+
+        }else{
+            textFinal += stringTextoPrincipal[k];
+            k++
+            j++
+        }
+    }
+
+    textSecundario.textContent = textFinal;
+    textPrincipal.value = '';
+}
