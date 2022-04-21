63823c4d2f109d47eb7a59657d52013574aa24cc
Michel Ferreira <michel.ferreira.melo@gmail.com>
2022-04-21 11:46:45 -0300

    pequenas mudanças após a criação do novo script inconsistencia.js

--- scripts/descriptografa.js
+++ scripts/descriptografa.js
@@ -3,40 +3,45 @@ function descriptografa(){
     var [j,k] = [0,0];
     var textFinal = '';
     var stringTextoPrincipal = textPrincipal.value
+    var inconsistencia = procuraInconsistencia(stringTextoPrincipal);
 
-    while(j < stringTextoPrincipal.length){
-        if(stringTextoPrincipal[k] === 'a'){
-            textFinal += 'a';
-            k += 2
-            j = k
+    if(!inconsistencia){
+        while(j < stringTextoPrincipal.length){
+            if(stringTextoPrincipal[k] === 'a'){
+                textFinal += 'a';
+                k += 2
+                j = k
 
-        }else if(stringTextoPrincipal[k] === 'e'){
-            textFinal += 'e';
-            k += 5
-            j = k
+            }else if(stringTextoPrincipal[k] === 'e'){
+                textFinal += 'e';
+                k += 5
+                j = k
 
-        }else if(stringTextoPrincipal[k] === 'i'){
-            textFinal += 'i';
-            k += 4
-            j = k
+            }else if(stringTextoPrincipal[k] === 'i'){
+                textFinal += 'i';
+                k += 4
+                j = k
 
-        }else if(stringTextoPrincipal[k] === 'o'){
-            textFinal += 'o';
-            k += 4
-            j = k
+            }else if(stringTextoPrincipal[k] === 'o'){
+                textFinal += 'o';
+                k += 4
+                j = k
 
-        }else if(stringTextoPrincipal[k] === 'u'){
-            textFinal += 'u';
-            k += 4
-            j = k
+            }else if(stringTextoPrincipal[k] === 'u'){
+                textFinal += 'u';
+                k += 4
+                j = k
 
-        }else{
-            textFinal += stringTextoPrincipal[k];
-            k++
-            j++
+            }else{
+                textFinal += stringTextoPrincipal[k];
+                k++
+                j++
+            }
         }
-    }
 
-    textSecundario.textContent = textFinal;
-    textPrincipal.value = '';
+        textSecundario.textContent = textFinal;
+        textPrincipal.value = '';
+    }else{
+        textPrincipal.value = "";
+    }
 }
